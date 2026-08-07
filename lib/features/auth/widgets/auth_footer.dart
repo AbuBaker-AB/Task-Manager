d4db4eb03d73428b81ef6518cd3db95d89ce39c9
Abu Baker <145045004+AbuBaker-AB@users.noreply.github.com>
2026-08-07 23:17:04 +0600

    Update auth_footer.dart

--- lib/features/auth/widgets/auth_footer.dart
+++ lib/features/auth/widgets/auth_footer.dart
@@ -1,45 +1,3 @@
-// import 'package:flutter/material.dart';
-//
-// import '../../../core/constants/app_colors.dart';
-// import '../../../core/constants/app_text_styles.dart';
-//
-// class AuthFooter extends StatelessWidget {
-//   final String text;
-//   final String actionText;
-//   final VoidCallback onTap;
-//
-//   const AuthFooter({
-//     super.key,
-//     required this.text,
-//     required this.actionText,
-//     required this.onTap,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       children: [
-//         Text(
-//           text,
-//           style: AppTextStyles.bodySecondary,
-//         ),
-//
-//         GestureDetector(
-//           onTap: onTap,
-//           child: Text(
-//             " $actionText",
-//             style: AppTextStyles.bodySecondary.copyWith(
-//               color: AppColors.primary,
-//               fontWeight: FontWeight.w600,
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 import '../../../core/constants/app_colors.dart';
@@ -62,7 +20,6 @@ class AuthFooter extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        // FIX: Changed color to textPrimary and added bold weight
         Text(
           text,
           style: AppTextStyles.bodySecondary.copyWith(
